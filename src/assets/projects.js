785d0654c7fe38b5791a3a97e1d733e995178d98
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-06-19 00:49:50 +0530

    Update projects.js

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -12,60 +12,66 @@ import project10_img from '../assets/project_10.jpg'
 const projects = [
     {
         w_no:1,
+        w_name:"Trendy Cart",
+        w_img:project10_img,
+        w_link:"https://trendy-cart.vercel.app/"
+    },
+    {
+        w_no:2,
         w_name:"Tasty Tap",
         w_img:project1_img,
         w_link:"https://tasty-tap-food-ordering-app.vercel.app/"
     },
     {
-        w_no:10,
+        w_no:11,
         w_name:"Personal Portfolio",
         w_img:project10_img,
         w_link:"https://aadarshportfolio.orangecom.xyz/"
     },
     {
-        w_no:2,
+        w_no:3,
         w_name:"Furniture Premium",
         w_img:project2_img,
         w_link:"https://furniture-premium.vercel.app/"
     },
     {
-        w_no:3,
+        w_no:4,
         w_name:"Resume Website",
         w_img:project3_img,
         w_link:"https://aadarshprofile.orangecom.xyz/"
     },
     {
-        w_no:4,
+        w_no:5,
         w_name:"Spotify Clone",
         w_img:project4_img,
         w_link:"https://spotifyclone.orangecom.xyz/"
     },
     {
-        w_no:5,
+        w_no:6,
         w_name:"Expense Tracker",
         w_img:project5_img,
         w_link:"https://expense-tracker-by-aadarsh.vercel.app/"
     },
     {
-        w_no:6,
+        w_no:7,
         w_name:"Employee Management System",
         w_img:project6_img,
         w_link:"https://github.com/Aadarsh032/Employee-Management-System-Project"
     },
     {
-        w_no:7,
+        w_no:8,
         w_name:"Sorting Visulaizer",
         w_img:project7_img,
         w_link:"https://sortingvisualizer.orangecom.xyz/"
     },
     {
-        w_no:8,
+        w_no:9,
         w_name:"Currency Converter",
         w_img:project8_img,
         w_link:"https://currency-converter-by-aadarsh.vercel.app/"
     },
     {
-        w_no:9,
+        w_no:10,
         w_name:"TIC-TAC-TOE",
         w_img:project9_img,
         w_link:"https://tic-tac-toe-by-aadarsh.vercel.app/"
