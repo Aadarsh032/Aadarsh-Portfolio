fd24a0ce3f740ebd176ed0b8800ed77d8ac46f21
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-06-19 15:27:10 +0530

    Update experiences_data.js

--- src/assets/experiences_data.js
+++ src/assets/experiences_data.js
@@ -1,4 +1,11 @@
 const experiences_data = [
+     {
+        company_name:"Freelancer",
+        designation:"Web Developerr",
+        date:"Apr, 2024 - Present",
+        achievement1:"Designed and coded a research papers website from scratch using HTML, CSS, JavaScript, and React.js for IIT-Hyderabad Professors enhancing their online presence, helping them gain more visibility.",
+        achievement2:"Delivered high-quality, responsive designs with up-to-date coding practices. Managed projects from initial consultation to post-launch support, ensuring client satisfaction."
+    },
     {
         company_name:"G7CR Technologies-A Noventiq Company",
         designation:"Cloud Engineer",
@@ -10,7 +17,7 @@ const experiences_data = [
     {
         company_name:"SKILLVERTEX",
         designation:"Business Development Associate",
-        date:"Jun, 2022 - Aug, 2023",
+        date:"Jun, 2022 - Aug, 2022",
         achievement1:"Cultivated relationships with potential clients through proactive cold calling and impactful meetings with renowned professors.",
         achievement2:"Secured crucial deals by leveraging these connections and demonstrating the value of our offerings.",
         achievement3:"Established a solid foundation for future collaborations, enhancing our business growth and reputation."
